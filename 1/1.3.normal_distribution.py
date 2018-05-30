c9976042e67dba17b052c7d0d85097bc41de562d
authorware <39698220+authorware@users.noreply.github.com>
2018-05-30 14:18:32 +0800

    Add files via upload

---
+++ 1/1.3.normal_distribution.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+# 数据
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+# np.random.randn(10000)随机生成期望为0，标准差为1的呈标准正态分布的10000个数
+# 为了更好的呈现效果，x加入了均值和标准差偏移
+x = mu + sigma * np.random.randn(10000)
+print(x)
+
+num_bins = 50
+# the histogram of the data
+n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='blue', alpha=0.5)
+
+# add a 'best fit' line
+y = mlab.normpdf(bins, mu, sigma)
+plt.plot(bins, y, 'r--')
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+plt.subplots_adjust(left=0.15)
+plt.show()
---
+++ 1/README.md
@@ -0,0 +1,3 @@
+# MachineLearning_zoubo
+小象学院—邹博，机器学习相关代码，
+基于python3
