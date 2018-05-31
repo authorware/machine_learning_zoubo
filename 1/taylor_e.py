53da02e6829bbbed3fee5c7ce2223ed805ec2298
authorware <39698220+authorware@users.noreply.github.com>
2018-05-31 23:04:45 +0800

    Add files via upload

---
+++ 1/taylor_e.py
@@ -0,0 +1,27 @@
+#自己写玩的
+#基于Taylor公式中，e的麦克劳林公式，算出一个值
+#后与numpy中的e比较
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def calc_n(n):
+    if n>1:
+        return calc_n(n-1)*n
+    else:
+        return 1
+
+def calc_e(x):
+    sum = 0
+    for i in x:
+        sum +=1/calc_n(i)
+    return sum
+
+
+
+if __name__ == '__main__':
+    x = [i for i in range(0,10)]
+    e_cal = calc_e(x)
+    #print(calc_n(4))
+    print(e_cal)
+    print(np.e)
